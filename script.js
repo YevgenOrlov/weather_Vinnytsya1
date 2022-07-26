92bbd110d1fdfa542a8f2784174bc5ed6d1375ca
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2022-07-26 20:03:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Vinnytsia,UA&appid=8b5388e2c898b936c5f2ce14fbad4939"
+)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    document.querySelector(".package-name").innerHTML = data.name;
+    document.querySelector(".price").innerHTML =
+      Math.round(data.main.temp - 273) + "&deg;";
+    document.querySelector(".disclaimer").textContent =
+      data.weather[0]["description"];
+    document.querySelector(
+      ".features li"
+    ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+  });
+// .catch(function(){})
